c9db829d575e4c07177d56aadf03d8a124070e11
Shrinidhi Shastri <shrinidhi@live.com>
2014-06-21 17:28:34 -0700

    First Draft

---
+++ Slides/js/slides.js
@@ -0,0 +1,45 @@
+var slide = 1;
+
+Slides = {
+    showSlide : function(ev) {
+        console.log("Show slide : " + history.state);
+        if (history.state != null && history.state != "") {
+            slide = history.state;
+            var px = "-" + $(window).height() * (slide - 1) + "";
+            $("#slides").animate({
+                top : px
+            });
+        }
+    },
+    handleKeyPress : function(ev) {
+        var height;
+        if (!$("#slides").is(":animated")) {
+            if (ev.keyCode == 39) {
+                if ($(document).height() > $(window).height()) {
+                    height = $(window).height();
+                    $("#slides").animate({
+                        top : "-=" + height
+                    });
+                    slide += 1;
+                    history.pushState(slide, "Slide " + slide, "/slide#"
+                            + slide);
+                }
+            } else if (ev.keyCode == 37) {
+                if ($("#slides").css("top") < "0px") {
+                    height = $(window).height();
+                    $("#slides").animate({
+                        top : "+=" + height
+                    });
+                    slide -= 1;
+                    history.pushState(slide, "Slide " + slide, "/slide#"
+                            + slide);
+                }
+            }
+        }
+    },
+    register : function() {
+        console.log("At load : " + history.state);
+        document.addEventListener("keyup", Slides.handleKeyPress);
+        window.addEventListener("popstate", this.showSlide);
+    }
+};
