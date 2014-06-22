97366d88502c97730ce993a9db1a0e162fa14b8b
Shrinidhi Shastri <shrinidhi@live.com>
2014-06-22 13:58:48 -0700

    Added touch and click handling

--- Slides/js/slides.js
+++ Slides/js/slides.js
@@ -1,45 +1,81 @@
-var slide = 1;
+(function(window, $) {
 
-Slides = {
-    showSlide : function(ev) {
-        if (history.state != null && history.state != "") {
-            slide = history.state;
-            var px = "-" + $(window).height() * (slide - 1) + "";
-            $("#slides").animate({
-                top : px
-            });
-        }
-    },
-    handleKeyPress : function(ev) {
-        var height;
-        if (!$("#slides").is(":animated")) {
-            if (ev.keyCode == 39) {
-                if ($(document).height() > $(window).height()) {
-                    height = $(window).height();
-                    $("#slides").animate({
-                        top : "-=" + height
-                    });
-                    slide += 1;
-                    history.pushState(slide, "Slide " + slide, "#"
-                            + slide);
-                }
-            } else if (ev.keyCode == 37) {
-                if ($("#slides").css("top") < "0px") {
-                    height = $(window).height();
-                    $("#slides").animate({
-                        top : "+=" + height
-                    });
-                    slide -= 1;
-                    history.pushState(slide, "Slide " + slide, "#"
-                            + slide);
+    'use strict';
+
+    var slide = 1;
+
+    window.Slides = function() {
+        this.initialize.apply(this, arguments);
+    };
+    window.Slides.prototype = {
+        initialize : function() {
+            this.setupEvents();
+            this.showSlide();
+        },
+        showSlide : function(ev) {
+            if (Modernizr.history && history.state != null
+                    && history.state != "") {
+                slide = history.state;
+                var px = "-" + $(window).height() * (slide - 1) + "";
+                $("#slides").animate({
+                    top : px
+                });
+            }
+        },
+        handleKeyPress : function(ev) {
+            if (!$("#slides").is(":animated")) {
+                if (ev.keyCode == 39 || ev.keyCode == 40) {
+                    // right or bottom arrow
+                    this.goForward();
+                } else if (ev.keyCode == 37 || ev.keyCode == 38) {
+                    // left or top arrow
+                    this.goBack();
                 }
             }
+        },
+        goBack : function() {
+            var height;
+            if ($("#slides").css("top") < "0px") {
+                height = $(window).height();
+                $("#slides").animate({
+                    top : "+=" + height
+                });
+                slide -= 1;
+                history.pushState(slide, "Slide " + slide, "#" + slide);
+            }
+        },
+        goForward : function() {
+            var height;
+            if ($(document).height() > $(window).height()) {
+                height = $(window).height();
+                $("#slides").animate({
+                    top : "-=" + height
+                });
+                slide += 1;
+                history.pushState(slide, "Slide " + slide, "#" + slide);
+            }
+        },
+        setupEvents : function() {
+            var self = this;
+            $(document).on("keyup", function(ev){self.handleKeyPress(ev)});
+            $(document).on("popstate", function(ev){self.showSlide(ev)});
+            $(document).on("click", function(evt) {
+                if (evt.pageY < ($(window).height() / 2)) {
+                    self.goBack();
+                } else {
+                    self.goForward();
+                }
+            });
+            
+            $(document).on("touchend", function(evt) {
+                var yPos = evt.originalEvent.changedTouches[0].pageY;
+                if (yPos < ($(window).height() / 2)) {
+                    self.goBack();
+                } else {
+                    self.goForward();
+                }
+            })
         }
-    },
-    register : function() {
-        console.log("At load : " + history.state);
-        document.addEventListener("keyup", Slides.handleKeyPress);
-        window.addEventListener("popstate", this.showSlide);
-        this.showSlide();
-    }
-};
+    };
+
+}(window, window.jQuery));
