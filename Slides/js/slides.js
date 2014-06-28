30e20ae428c0463725864f69a813e389d7c16fb2
Shrinidhi Shastri <shrinidhi@live.com>
2014-06-28 15:16:32 -0700

    De-Lint

--- Slides/js/slides.js
+++ Slides/js/slides.js
@@ -1,3 +1,10 @@
+/**
+ * @author Shrinidhi Shastri
+ * @param window
+ * @param $
+ *            jQuery object
+ */
+// Self executing anonymous function
 (function(window, $) {
 
     'use strict';
@@ -6,11 +13,21 @@
         this.initialize.apply(this, arguments);
     };
     window.Slides.prototype = {
+        /**
+         * Initialize the slide show
+         * 
+         * @method initialize
+         */
         initialize : function() {
             this.slide = 1;
             this.setupEvents();
             this.goToCurrentSlide();
         },
+        /**
+         * Sets up events to handle slide navigation
+         * 
+         * @method setupEvents
+         */
         setupEvents : function() {
             var self, $win;
             self = this;
@@ -22,47 +39,70 @@
                 self.goToSlide(ev.originalEvent.state, false);
             });
             $win.on("click", function(evt) {
-                if (evt.clientY < ($(window).height() / 2)) {
+                if (evt.clientX < ($(window).width() / 2)) {
                     self.goBack();
                 } else {
                     self.goForward();
                 }
             });
             $win.on("touchend", function(evt) {
-                var yPos = evt.originalEvent.changedTouches[0].clientY;
-                if (yPos < ($(window).height() / 2)) {
+                var xPos = evt.originalEvent.changedTouches[0].clientX;
+                if (xPos < ($(window).width() / 2)) {
                     self.goBack();
                 } else {
                     self.goForward();
                 }
             });
         },
+        /**
+         * Handles ArrowKeys and Enter key press events.
+         * 
+         * @method handleKeyPress
+         */
         handleKeyPress : function(ev) {
             if (!$("#slides").is(":animated")) {
-                if (ev.keyCode == 39 || ev.keyCode == 40 || ev.keyCode == 13) {
+                if (ev.keyCode === 39 || ev.keyCode === 40 || ev.keyCode === 13) {
                     // right or bottom arrow or Enter
                     this.goForward();
-                } else if (ev.keyCode == 37 || ev.keyCode == 38) {
+                } else if (ev.keyCode === 37 || ev.keyCode === 38) {
                     // left or top arrow
                     this.goBack();
                 }
             }
         },
+        /**
+         * Displays the current slide as indicated by the hash location, else
+         * goes to slide 1
+         * 
+         * @method goToCurrentSlide
+         */
         goToCurrentSlide : function() {
             var toSlide, hashStr;
             toSlide = 1;
             hashStr = window.location.hash;
-            console.log(hashStr);
             if (hashStr && !isNaN(hashStr.substring(1))) {
                 toSlide = parseInt(hashStr.substring(1));
             }
             this.goToSlide(toSlide);
         },
+        /**
+         * Displays a slide indicated by toSlide. If toSlide is out of bounds,
+         * displays current slide.
+         * 
+         * @method goToSlide
+         * @param toSlide
+         *            {Number} Slide number to go to
+         * @param pushState
+         *            {Boolean} To prevent adding the state to the browser
+         *            history.
+         * 
+         */
         goToSlide : function(toSlide, pushState) {
-            if(pushState === undefined) {
+            var selector;
+            if (pushState === undefined) {
                 pushState = true;
             }
-            if(!toSlide) {
+            if (!toSlide) {
                 this.goToCurrentSlide();
             }
 
@@ -70,20 +110,30 @@
                     || toSlide > $('.slideWrapper').size()) {
                 return;
             }
-            var selector, el;
+
             $(".selected").removeClass('selected');
             selector = ".slideWrapper:eq(" + (toSlide - 1) + ")";
             $(selector).addClass("selected");
             this.slide = toSlide;
-            if(pushState) {
+            if (pushState) {
                 history.pushState(toSlide, "Slide " + toSlide, "#" + toSlide);
             }
         },
+        /**
+         * Goes back 1 slide.
+         * 
+         * @method goBack
+         */
         goBack : function() {
             var toSlide;
             toSlide = this.slide - 1;
             this.goToSlide(toSlide);
         },
+        /**
+         * Goes forward 1 slide.
+         * 
+         * @method goForward
+         */
         goForward : function() {
             var toSlide;
             toSlide = this.slide + 1;
