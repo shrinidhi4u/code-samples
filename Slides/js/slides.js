9b2dd251e0e2f87c0379813c1da03c23e7b53239
Shrinidhi Shastri <shrinidhi@live.com>
2014-06-28 15:00:53 -0700

    Version 2

--- Slides/js/slides.js
+++ Slides/js/slides.js
@@ -2,60 +2,45 @@
 
     'use strict';
 
-    var slide = 1;
-
     window.Slides = function() {
         this.initialize.apply(this, arguments);
     };
     window.Slides.prototype = {
         initialize : function() {
+            this.slide = 1;
             this.setupEvents();
-            this.showSlide();
+            this.goToCurrentSlide();
         },
         setupEvents : function() {
-            var self = this;
-            $(document).on("keyup", function(ev) {
-                self.handleKeyPress(ev)
+            var self, $win;
+            self = this;
+            $win = $(window);
+            $win.on("keyup", function(ev) {
+                self.handleKeyPress(ev);
             });
-            $(document).on("popstate", function(ev) {
-                self.showSlide(ev)
+            $win.on("popstate", function(ev) {
+                self.goToSlide(ev.originalEvent.state, false);
             });
-            $(document).on("click", function(evt) {
-                if (evt.pageY < ($(window).height() / 2)) {
+            $win.on("click", function(evt) {
+                if (evt.clientY < ($(window).height() / 2)) {
                     self.goBack();
                 } else {
                     self.goForward();
                 }
             });
-            $(document).on("touchend", function(evt) {
-                var yPos = evt.originalEvent.changedTouches[0].pageY;
+            $win.on("touchend", function(evt) {
+                var yPos = evt.originalEvent.changedTouches[0].clientY;
                 if (yPos < ($(window).height() / 2)) {
                     self.goBack();
                 } else {
                     self.goForward();
                 }
             });
-            $(window).resize(function(ev) {
-                clearTimeout($.data(this, 'resizeTimer'));
-                $.data(this, 'resizeTimer', setTimeout(function() {
-                    self.showSlide(ev);
-                }, 250));
-            });
-        },
-        showSlide : function(ev) {
-            if (Modernizr.history && history.state != null
-                    && history.state != "") {
-                slide = history.state;
-                var px = "-" + $(window).height() * (slide - 1) + "";
-                $("#slides").animate({
-                    top : px
-                });
-            }
         },
         handleKeyPress : function(ev) {
             if (!$("#slides").is(":animated")) {
-                if (ev.keyCode == 39 || ev.keyCode == 40) {
-                    // right or bottom arrow
+                if (ev.keyCode == 39 || ev.keyCode == 40 || ev.keyCode == 13) {
+                    // right or bottom arrow or Enter
                     this.goForward();
                 } else if (ev.keyCode == 37 || ev.keyCode == 38) {
                     // left or top arrow
@@ -63,27 +48,46 @@
                 }
             }
         },
-        goBack : function() {
-            var height;
-            if ($("#slides").css("top") < "0px") {
-                height = $(window).height();
-                $("#slides").animate({
-                    top : "+=" + height
-                });
-                slide -= 1;
-                history.pushState(slide, "Slide " + slide, "#" + slide);
+        goToCurrentSlide : function() {
+            var toSlide, hashStr;
+            toSlide = 1;
+            hashStr = window.location.hash;
+            console.log(hashStr);
+            if (hashStr && !isNaN(hashStr.substring(1))) {
+                toSlide = parseInt(hashStr.substring(1));
+            }
+            this.goToSlide(toSlide);
+        },
+        goToSlide : function(toSlide, pushState) {
+            if(pushState === undefined) {
+                pushState = true;
+            }
+            if(!toSlide) {
+                this.goToCurrentSlide();
+            }
+
+            if (isNaN(toSlide) || toSlide < 1
+                    || toSlide > $('.slideWrapper').size()) {
+                return;
+            }
+            var selector, el;
+            $(".selected").removeClass('selected');
+            selector = ".slideWrapper:eq(" + (toSlide - 1) + ")";
+            $(selector).addClass("selected");
+            this.slide = toSlide;
+            if(pushState) {
+                history.pushState(toSlide, "Slide " + toSlide, "#" + toSlide);
             }
         },
+        goBack : function() {
+            var toSlide;
+            toSlide = this.slide - 1;
+            this.goToSlide(toSlide);
+        },
         goForward : function() {
-            var height;
-            if ($(document).height() > $(window).height()) {
-                height = $(window).height();
-                $("#slides").animate({
-                    top : "-=" + height
-                });
-                slide += 1;
-                history.pushState(slide, "Slide " + slide, "#" + slide);
-            }
+            var toSlide;
+            toSlide = this.slide + 1;
+            this.goToSlide(toSlide);
         }
     };
 
